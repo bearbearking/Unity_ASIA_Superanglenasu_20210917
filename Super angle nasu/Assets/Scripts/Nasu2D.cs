85b197a2497b1d0a21635eb0d6dab7cae2f78c0c
bearbearking <90890397+bearbearking@users.noreply.github.com>
2021-12-03 15:53:29 +0800

    Update角色跳躍

--- Super angle nasu/Assets/Scripts/Nasu2D.cs
+++ Super angle nasu/Assets/Scripts/Nasu2D.cs
@@ -16,11 +16,23 @@ public class Nasu2D : MonoBehaviour
     [Header("跳躍按鍵與可跳躍圖層")]
     public KeyCode keyJump = KeyCode.Space;
     public LayerMask canJumpLayer;
+    [Header("動畫參數:走路與跳躍")]
+    public string parameterWalk = "走路開關";
+    public string parameterJump = "跳躍開關";
     #endregion
 
+    #region  欄位:私人
     // Start is called before the first frame update
+    private Animator ani;
     private Rigidbody2D rig;
 
+    [SerializeField]
+
+    private bool isGrounded;
+
+    #endregion
+
+    #region 事件
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0.2f, 0.3f);
@@ -30,23 +42,29 @@ public class Nasu2D : MonoBehaviour
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
     private void Update()
     {
         Flip();
+        CheckGround();
+        Jump();
     }
     // Update is called once per frame
     private void FixedUpdate()
     {
         Move();
     }
+    #endregion
+
     #region 方法
     private void Move()
     {
         float h = Input.GetAxis("Horizontal");
-        print("玩家左右按鍵值:" + h);
+        //print("玩家左右按鍵值:" + h);
 
         rig.velocity = new Vector2(h * speed, rig.velocity.y);
+        ani.SetBool(parameterWalk, h != 0);
     }
 
     private void Flip()
@@ -60,6 +78,25 @@ public class Nasu2D : MonoBehaviour
         else if(h > 0)
         {
             transform.eulerAngles = Vector3.zero;
+        }
+    }
+
+    private void CheckGround()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position +
+            transform.TransformDirection(checkGroundOffset), checkGroundRadius, canJumpLayer);
+
+        //print("碰到的物件名稱:" + hit.name);
+        
+        isGrounded = hit;
+
+        ani.SetBool(parameterJump, !isGrounded);
+    }
+    private void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(keyJump))
+        {
+            rig.AddForce(new Vector2(0, jump));
         }
     }
     #endregion
